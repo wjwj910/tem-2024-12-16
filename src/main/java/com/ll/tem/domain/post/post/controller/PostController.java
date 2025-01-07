21e319bfd48c2594778ce90f7dfa9ab75c24a5d0
wjwj910 <wjwj910@gmail.com>
2025-01-07 15:11:28 +0900

    PostWriteForm 1

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -1,13 +1,13 @@
 package com.ll.tem.domain.post.post.controller;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -31,16 +31,26 @@ public class PostController {
         return getFormHtml("","","");
     }
 
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    public static class PostWriteForm {
+        @NotBlank(message = "제목을 입력해주세요.")
+        @Length(min = 5, message = "제목을 5자 이상 입력해주세요.")
+        private String title;
+
+        @NotBlank(message = "내용을 입력해주세요.")
+        @Length(min = 10, message = "내용을 10자 이상 입력해주세요.")
+        private String content;
+    }
+
     @PostMapping("/write")
     @ResponseBody
     public String write(
-            @NotBlank(message = "제목을 입력해주세요.")
-            @Length(min = 5, message = "제목을 5자 이상 입력해주세요.")
-            String title,
-            @NotBlank(message = "내용을 입력해주세요.")
-            @Length(min = 10, message = "내용을 10자 이상 입력해주세요.")
-            String content
+            @ModelAttribute PostWriteForm form
     ) {
+        System.out.println("form = " + form);
+
         return """
                 <h1>글쓰기 완료</h1>
                 
@@ -48,6 +58,6 @@ public class PostController {
                     <h2>%s</h2>
                     <p>%s</p>
                 </div>
-                """.formatted(title, content);
+                """.formatted(form.getTitle(), form.getContent());
     }
 }
