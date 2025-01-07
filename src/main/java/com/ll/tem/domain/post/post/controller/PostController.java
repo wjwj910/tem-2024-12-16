fdda59fd27ddd56f20d9e3c177e21e36379e07a4
wjwj910 <wjwj910@gmail.com>
2025-01-07 14:39:10 +0900

    dry

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -9,16 +9,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+    private  String getFormHtml(String errorMessage, String title, String content) {
+        return """
+                <div>%s</div>
+                <form method="POST">
+                        <input type="text" name="title" placeholder="제목" value="%s">
+                        <br>
+                        <textarea name="content" placeholder="내용">%s</textarea>
+                        <br>
+                        <button type="submit">글쓰기</button>
+                    </form>
+               """.formatted(errorMessage, title, content);
+    }
     @GetMapping("/write")
     @ResponseBody
     public String showWrite() {
-        return """
-                <form method="POST">
-                    <input type="text" name="title" placeholder="제목">
-                    <textarea name="content" placeholder="내용"></textarea>
-                    <button type="submit">글쓰기</button>
-                </form>
-                """;
+        return getFormHtml("","","");
     }
 
     @PostMapping("/write")
@@ -28,47 +34,19 @@ public class PostController {
             String content
     ) {
         if (title == null || title.isBlank()) {
-            return """
-                    <div>%s</div>
-                    <form method="POST">
-                        <input type="text" name="title" placeholder="제목">
-                        <textarea name="content" placeholder="내용"></textarea>
-                        <button type="submit">글쓰기</button>
-                    </form>
-                    """.formatted("제목을 입력하세요");
+            return getFormHtml("제목을 입력해주세요.", title, content);
         }
 
         if (title.length() < 5) {
-            return """
-                    <div>%s</div>
-                    <form method="POST">
-                        <input type="text" name="title" placeholder="제목" value="%s">
-                        <textarea name="content" placeholder="내용"></textarea>
-                        <button type="submit">글쓰기</button>
-                    </form>
-                    """.formatted("제목을 5자 이상 입력하세요.", title);
+            return getFormHtml("제목을 5자 이상 입력해주세요.", title, content);
         }
 
         if (content == null || content.isBlank()) {
-            return """
-                    <div>%s</div>
-                    <form method="POST">
-                        <input type="text" name="title" placeholder="제목" value="%s">
-                        <textarea name="content" placeholder="내용"></textarea>
-                        <button type="submit">글쓰기</button>
-                    </form>
-                    """.formatted("내용을 입력하세요", title);
+            return getFormHtml("내용을 입력해주세요.", title, content);
         }
 
         if (content.length() < 10) {
-            return """
-                    <div>%s</div>
-                    <form method="POST">
-                        <input type="text" name="title" placeholder="제목" value="%s">
-                        <textarea name="content" placeholder="내용">%s</textarea>
-                        <button type="submit">글쓰기</button>
-                    </form>
-                    """.formatted("내용을 10자 이상 입력해주세요", title, content);
+            return getFormHtml("내용을 10자 이상 입력해주세요.", title, content);
         }
 
         return """
