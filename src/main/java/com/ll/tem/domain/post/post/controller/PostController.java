09a74621a4ad497bdf16692a91f4aff505ec1d55
wjwj910 <wjwj910@gmail.com>
2025-01-07 14:44:22 +0900

    validated

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.ll.tem.domain.post.post.controller;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/posts")
+@Validated
 public class PostController {
     private  String getFormHtml(String errorMessage, String title, String content) {
         return """
@@ -30,25 +34,9 @@ public class PostController {
     @PostMapping("/write")
     @ResponseBody
     public String write(
-            String title,
-            String content
+            @NotBlank @Length(min = 5) String title,
+            @NotBlank @Length(min = 10) String content
     ) {
-        if (title == null || title.isBlank()) {
-            return getFormHtml("제목을 입력해주세요.", title, content);
-        }
-
-        if (title.length() < 5) {
-            return getFormHtml("제목을 5자 이상 입력해주세요.", title, content);
-        }
-
-        if (content == null || content.isBlank()) {
-            return getFormHtml("내용을 입력해주세요.", title, content);
-        }
-
-        if (content.length() < 10) {
-            return getFormHtml("내용을 10자 이상 입력해주세요.", title, content);
-        }
-
         return """
                 <h1>글쓰기 완료</h1>
                 
