31b6a1bc46ef674b2d6ea4b75b261f3ba4c6855a
wjwj910 <wjwj910@gmail.com>
2025-01-07 12:43:19 +0900

    settings

---
+++ src/main/java/com/ll/tem/TemApplication.java
@@ -0,0 +1,13 @@
+package com.ll.tem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/tem/TemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.tem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
