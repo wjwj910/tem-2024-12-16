49ed0e87e5d666ccd446258af844703abc8c577c
wjwj910 <wjwj910@gmail.com>
2025-01-07 14:07:47 +0900

    write

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -12,11 +12,27 @@ public class PostController {
     @ResponseBody
     public String showWrite() {
         return """
-                <form>
+                <form action="/posts/doWrite">
                     <input type="text" name="title" placeholder="제목">
                     <textarea name="content" placeholder="내용"></textarea>
                     <button type="submit">글쓰기</button>
                 </form>
                 """;
+    }
+
+    @GetMapping("/doWrite")
+    @ResponseBody
+    public String write(
+            String title,
+            String content
+    ) {
+        return """
+                <h1>글쓰기 완료</h1>
+                
+                <div>
+                    <h2>%s</h2>
+                    <p>%s</p>
+                </div>
+                """.formatted(title, content);
     }
 }
