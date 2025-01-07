2b451e94297bc6aa4ae41696185177581023af98
wjwj910 <wjwj910@gmail.com>
2025-01-07 16:47:01 +0900

    write.html 1

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -4,7 +4,6 @@ import com.ll.tem.domain.post.post.entity.Post;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,43 +22,30 @@ public class PostController {
     private List<Post> posts = new ArrayList<>() {{
         add(
                 Post.builder()
-                        .title("제목1")
-                        .content("내용1")
-                        .build()
+                    .title("제목1")
+                    .content("내용1")
+                    .build()
         );
 
         add(
                 Post.builder()
-                        .title("제목2")
-                        .content("내용2")
-                        .build()
+                    .title("제목2")
+                    .content("내용2")
+                    .build()
         );
 
         add(
                 Post.builder()
-                        .title("제목3")
-                        .content("내용3")
-                        .build()
+                    .title("제목3")
+                    .content("내용3")
+                    .build()
         );
     }};
-    private  String getFormHtml(String errorMessage, String title, String content) {
-        return """
-                <div>%s</div>
-                <form method="POST">
-                        <input type="text" name="title" placeholder="제목" value="%s">
-                        <br>
-                        <textarea name="content" placeholder="내용">%s</textarea>
-                        <br>
-                        <button type="submit">글쓰기</button>
-                    </form>
-               """.formatted(errorMessage, title, content);
-    }
 
     @GetMapping
     @ResponseBody
     public String showList() {
-        String ul = "<ul>" + posts
-                .reversed()
+        String ul = "<ul>" + posts.reversed()
                 .stream()
                 .map(post -> "<li>%s</li>".formatted(post.getTitle()))
                 .collect(Collectors.joining()) + "</ul>";
@@ -87,36 +73,21 @@ public class PostController {
             @NotBlank(message = "03-내용을 입력해주세요.")
             @Length(min = 10, message = "04-내용을 10자 이상 입력해주세요.")
             String content
-    ) {
-    }
+    ) {}
 
     @PostMapping("/write")
-    public ResponseEntity<String> write(
-            @Valid PostWriteForm form,
-            BindingResult bindingResult
-    ) {
+    public String write(
+            @Valid PostWriteForm form, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            String errorMessages = bindingResult.getAllErrors()
-                    .stream()
-                    .map(error -> error.getDefaultMessage())
-                    .sorted()
-                    .map(message -> message.split("-", 2)[1])
-                    .collect(Collectors.joining("<br>"));
-
-            return ResponseEntity
-                    .badRequest() // 400
-                    .body(getFormHtml(errorMessages, form.title, form.content));
+            return "domain/post/post/write";
         }
         posts.add(
                 Post.builder()
-                        .title(form.title)
-                        .content(form.content)
-                        .build()
+                    .title(form.title)
+                    .content(form.content)
+                    .build()
         );
 
-        return ResponseEntity
-                .status(302) // 다른 곳으로 즉시 이동하시오.
-                .header("Location", "/posts")
-                .build();
+        return "redirect:/posts";
     }
 }
